53f8ffb3d55eb31c928647b0b74f43bcd10dbdc0
Sachin <sachin.nawale2063@gmail.com>
2024-04-02 19:23:44 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,13 @@ import MessageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 import connectToMongoDB from './db/connectToMongoDB.js';
-import {app, server} from'./socket/socket.js'
+import { app, server } from './socket/socket.js'
 
+import path from 'path'
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,12 +24,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", MessageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res)=>{
-//     res.send("Hello World!")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
-server.listen(PORT, async ()=>{
+
+server.listen(PORT, async () => {
     await connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
 });
